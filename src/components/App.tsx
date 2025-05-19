da808d5b47b593feca2d273471e4d7bab91d249c
Paul Husek <paulhusek@hotmail.com>
2025-05-19 10:58:53 -0400

    Populated Page with Contents

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,15 @@
+import Background from "./Background.tsx";
+import Header from "./Header.tsx";
+import Container from "./Container.tsx";
+import Footer from "./Footer.tsx";
+
 function App() {
-  return <>Hello!</>;
+  return <>
+    <Background />
+    <Header />
+    <Container />
+    <Footer />
+  </>;
 }
 
 export default App;
